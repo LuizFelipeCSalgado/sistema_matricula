737805bd18a222cd7228ee5f496fdce8b1f63989
Luiz Felipe Salgado <luizfelipecsalgado@gmail.com>
2020-05-12 22:30:05 -0300

    continuação README.md e infrastructure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# sistema_matricula
+# sistema\_matricula
 POC sistema de matrículas usando Clean Architecture
 
 
@@ -6,11 +6,28 @@ Para o sistema foi projetado que tenha as seguintes camadas e padrões:
 - Entity
 - Use Cases
 - DAO
+- Service
 - Controller -> API RESTful
+
+## Entity
+Contém classes que encapsulam dados que são expostos segundo **as regras
+de negócio da empresa**.
+
+## Use Cases
+Contém funções (puras) que utilizam as regras de negócio. São responsáveis
+por conter as regras de negócio da aplicação.
+1) Criar um pedido de matrícula
+2) Deferir um pedido de matrícula
+3) Verificar se um pedido de matrícula foi aprovado
+
+## DAO
+## Service
+## Controller
 
 Poderia ser implementado em um próximo passo uma camada de *service*, reponsável por chamar
 os *use cases*, diminuindo o acoplamento entre o *controller* e *use cases*.
 
 
-Para rodar o projeto é preciso setar PYTHONPATH com o presente diretório.
+- pip install -r requirements.txt
+- setar PYTHONPATH com o presente diretório.
   set PYTHONPATH=$PYTHONPATH:$(pwd)
--- infrastructure/daos.py
+++ infrastructure/daos.py
@@ -1,11 +1,11 @@
-import connection
+from entities.aluno import Aluno
 
 def escrever_objeto_aluno(aluno_raw: dict) -> Aluno:
     return Aluno()
 
 class AlunoDAO:
-    def __init__(self):
-        self.cursor = connection.get_cursor()
+    def __init__(self, cursor):
+        self.cursor = cursor
 
     def get_all(self):
         query = 'SELECT * FROM alunos'
@@ -13,3 +13,23 @@ class AlunoDAO:
         query_result = cursor.fetchall()
         import pdb; pdb.set_trace()
         return map(escrever_objeto_aluno, query_result)
+
+    def create(self, aluno: Aluno) -> bool:
+        query = f"""
+            INSERT INTO sistema_matriculas.alunos (
+                nome,
+                curso,
+                disciplinas_matriculadas,
+                disciplinas_requisitadas
+            ) VALUES (
+                '{aluno.nome}',
+                '{aluno.curso}',
+                '{aluno.disciplinas_matriculadas}',
+                '{aluno.disciplinas_requisitadas}'
+            );
+        """
+        self.cursor.execute(query)
+        return True
+
+    def get_aluno(id: int) -> Aluno:
+        pass
